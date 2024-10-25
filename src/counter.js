7415318ac98ff04497b79511a859a623f43d6d79
= <alexey2435ytgh@gmail.com>
2024-10-26 02:17:53 +0300

    first commit

---
+++ src/counter.js
@@ -0,0 +1,11 @@
+export function increment(count) {
+    return count + 1;
+}
+
+export function decrement(count) {
+    return count - 1;
+}
+
+export function getCountDisplay(count) {
+    return `Счётчик: ${count}`;
+}
